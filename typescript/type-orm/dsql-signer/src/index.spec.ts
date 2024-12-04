f4d3b6d7236033ce8f75b1aa06ec28e4880dbf22
Ian Forster <forstaia@amazon.com>
2024-12-04 08:17:28 -0800

    Remove typescript

--- typescript/type-orm/dsql-signer/src/index.spec.ts
+++
@@ -1,9 +0,0 @@
-import { describe, expect, test as it } from "vitest";
-
-import * as dsql from "./index";
-
-describe("dsql-signer", () => {
-  it("has Signer", () => {
-    expect(dsql.DsqlSigner).toBeDefined();
-  });
-});
