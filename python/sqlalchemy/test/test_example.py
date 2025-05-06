d7b043fbfb4b7dc5ec86e905b13f652b3cfe0044
Mitchell Elholm <mitchell.elholm@improving.com>
2025-05-06 12:37:19 -0700

    Update SQLAlchemy example to include non-admin and connection refresh logic (#119)
    * Update SQLAlchemy example to include non-admin and connection refresh logic
    
    * Fix github action
    
    ---------
    
    Co-authored-by: Mitchell Elholm <elholmit@amazon.com>

--- python/sqlalchemy/test/test_example.py
+++ python/sqlalchemy/test/test_example.py
@@ -1,16 +1,11 @@
-from example import main
+from example import example, run_retry
 import pytest
-import os, sys
+import os
 
 # Smoke tests that our example works fine
 def test_example():
     try:
-        cluster_endpoint = os.environ.get("CLUSTER_ENDPOINT", None)
-        region = os.environ.get("REGION", None)
-        if cluster_endpoint is None:
-            sys.exit("CLUSTER_ENDPOINT environment variable is not set")
-        if region is None:
-            sys.exit("REGION environment variable is not set")
-        main(cluster_endpoint, region)
+        example()
+        run_retry()
     except Exception as e:
-        pytest.fail("Unexpected exception: " + str(e))
+        pytest.fail(f"Unexpected exception: {e}")
