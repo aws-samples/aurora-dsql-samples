c335a05a9895902b14c7759f5b2f9f833c66fac9
Leszek <77356733+leszek-bq@users.noreply.github.com>
2025-05-06 17:35:16 -0700

    Update Ruby-pg samples and README.md (#117)
    * Update the Ruby-pg example. Include non-admin scenario
    
    * added CLUSTER_USER env variable to gh
    
    * added certificate download
    
    * addressing review comments
    
    * updated minimum ruby version to 3+
    
    * Updated ruby version to 3.3 for integration tests
    
    ---------
    
    Co-authored-by: Leszek Kurzyna <lkurzyna@amazon.com>

--- ruby/ruby-pg/spec/smoke_spec.rb
+++ ruby/ruby-pg/spec/smoke_spec.rb
@@ -2,9 +2,8 @@ require 'hello_dsql'
 
 describe 'perform smoke tests' do 
     it 'does not raise any exception' do 
-
         expect {
-            example(ENV["CLUSTER_ENDPOINT"], ENV["REGION"])
+            main()
         }.not_to raise_error
     end
 end 
