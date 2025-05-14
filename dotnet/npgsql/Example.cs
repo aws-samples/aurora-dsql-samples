47e7c175a6cc1fd88c3ad4cb25937682b127902d
Daniel Frankcom <daniel@frankcom.ca>
2025-05-14 16:34:22 -0700

    Update .NET example to include non-admin logic and separated auth (#132)

--- dotnet/npgsql/Example.cs
+++ dotnet/npgsql/Example.cs
@@ -1,57 +1,123 @@
-﻿using Npgsql;
+﻿using System.Diagnostics;
 using Amazon;
 using Amazon.DSQL.Util;
 using Amazon.Runtime;
+using Amazon.Runtime.Credentials;
+using Npgsql;
 
-class Example
+namespace example;
+
+internal static class Example
 {
-    public static async Task Run(string clusterEndpoint)
+    private static async Task<NpgsqlConnection> CreateConnection(
+        string clusterUser, string clusterEndpoint, string regionName)
     {
-        RegionEndpoint region = RegionEndpoint.USEast1;
+        var region = RegionEndpoint.GetBySystemName(regionName);
 
-        // Connect to a PostgreSQL database.
-        const string username = "admin";
-        
-        // The token expiration time is optional, and the default value 900 seconds
-        AWSCredentials awsCredentials = FallbackCredentialsFactory.GetCredentials();
-        string password = DSQLAuthTokenGenerator.GenerateDbConnectAdminAuthToken(awsCredentials, region, clusterEndpoint);
-        
-        const string database = "postgres";
-        var connString = "Host=" + clusterEndpoint + ";Username=" + username + ";Password=" + password + ";Database=" + database + ";Port=" + 5432 + ";SSLMode=VerifyFull;";
+        AWSCredentials awsCredentials = await DefaultAWSCredentialsIdentityResolver.GetCredentialsAsync();
 
-        var conn = new NpgsqlConnection(connString);
+        // Generate a fresh password token for each connection, to ensure the token is not expired when the connection
+        // is established
+        string password;
+        string schema;
+        if (clusterUser == "admin")
+        {
+            password = await DSQLAuthTokenGenerator.GenerateDbConnectAdminAuthTokenAsync(
+                awsCredentials, region, clusterEndpoint);
+            schema = "public";
+        }
+        else
+        {
+            password =
+                await DSQLAuthTokenGenerator.GenerateDbConnectAuthTokenAsync(awsCredentials, region, clusterEndpoint);
+            schema = "myschema";
+        }
+
+        var connBuilder = new NpgsqlConnectionStringBuilder
+        {
+            Host = clusterEndpoint,
+            Port = 5432,
+            SslMode = SslMode.VerifyFull,
+            Database = "postgres",
+            Username = clusterUser,
+            Password = password,
+            IncludeErrorDetail = true
+        };
+
+        var conn = new NpgsqlConnection(connBuilder.ConnectionString);
         await conn.OpenAsync();
 
-        // Create a table.
-        using var create = new NpgsqlCommand("CREATE TABLE IF NOT EXISTS owner (id UUID PRIMARY KEY, name VARCHAR(30) NOT NULL, city VARCHAR(80) NOT NULL, telephone VARCHAR(20))", conn);
+        try
+        {
+            using var setSearchPath = new NpgsqlCommand($"SET search_path = {schema}", conn);
+            setSearchPath.ExecuteNonQuery();
+        }
+        catch
+        {
+            await conn.CloseAsync();
+            throw;
+        }
+
+        return conn;
+    }
+
+    private static async Task ExerciseConnection(NpgsqlConnection conn)
+    {
+        using var create =
+            new NpgsqlCommand(@"
+                CREATE TABLE IF NOT EXISTS owner (
+                    id UUID NOT NULL DEFAULT gen_random_uuid(),
+                    name VARCHAR(30) NOT NULL,
+                    city VARCHAR(80) NOT NULL,
+                    telephone VARCHAR(20) DEFAULT NULL,
+                    PRIMARY KEY (id)
+                )",
+                conn);
         create.ExecuteNonQuery();
 
-        // Create an owner.
-        var uuid = Guid.NewGuid();
-        using var insert = new NpgsqlCommand("INSERT INTO owner(id, name, city, telephone) VALUES(@id, @name, @city, @telephone)", conn);
-        insert.Parameters.AddWithValue("id", uuid);
+        // Insert some data
+        using var insert = new NpgsqlCommand(
+            "INSERT INTO owner(name, city, telephone) VALUES(@name, @city, @telephone)", conn);
         insert.Parameters.AddWithValue("name", "John Doe");
         insert.Parameters.AddWithValue("city", "Anytown");
-
-        insert.Parameters.AddWithValue("telephone", "555-555-0190");
-
+        insert.Parameters.AddWithValue("telephone", "555-555-1999");
         insert.ExecuteNonQuery();
 
-        // Read the owner.
-        using var select = new NpgsqlCommand("SELECT * FROM owner where id=@id", conn);
-        select.Parameters.AddWithValue("id", uuid);
-        using var reader = await select.ExecuteReaderAsync();
-        System.Diagnostics.Debug.Assert(reader.HasRows, "no owner found");
+        using var select = new NpgsqlCommand("SELECT * FROM owner where name=@name", conn);
+        select.Parameters.AddWithValue("name", "John Doe");
+        await using var reader = await select.ExecuteReaderAsync();
 
-        System.Diagnostics.Debug.WriteLine(reader.Read());
+        Debug.Assert(reader.HasRows, "no owner found");
+        await reader.ReadAsync();
 
-        reader.Close();
+        // Verify the result we got is what we inserted before
+        Debug.Assert(!reader.IsDBNull(reader.GetOrdinal("id")), "id is null");
+        Debug.Assert(reader.GetString(reader.GetOrdinal("name")) == "John Doe", "name doesn't match");
+        Debug.Assert(reader.GetString(reader.GetOrdinal("city")) == "Anytown", "city doesn't match");
+        Debug.Assert(reader.GetString(reader.GetOrdinal("telephone")) == "555-555-1999", "telephone doesn't match");
+        await reader.CloseAsync();
 
-        using var delete = new NpgsqlCommand("DELETE FROM owner where id=@id", conn);
-        select.Parameters.AddWithValue("id", uuid);
+        // Clean up the table after the example. If we run the example again we do not have to worry about data inserted
+        // by previous runs
+        using var delete = new NpgsqlCommand("DELETE FROM owner where name=@name", conn);
+        select.Parameters.AddWithValue("name", "John Doe");
         select.ExecuteNonQuery();
+    }
 
-        // Close the connection.
-        conn.Close();
+    public static async Task Main()
+    {
+        var clusterUser = Environment.GetEnvironmentVariable("CLUSTER_USER");
+        Debug.Assert(clusterUser != null, "CLUSTER_USER must be set");
+
+        var clusterEndpoint = Environment.GetEnvironmentVariable("CLUSTER_ENDPOINT");
+        Debug.Assert(clusterEndpoint != null, "CLUSTER_ENDPOINT must be set");
+
+        var region = Environment.GetEnvironmentVariable("REGION");
+        Debug.Assert(region != null, "REGION must be set");
+
+        await using var conn = await CreateConnection(clusterUser!, clusterEndpoint!, region!);
+        await ExerciseConnection(conn);
+
+        Console.WriteLine("Connection exercised successfully");
     }
 }
--- dotnet/npgsql/ExampleTest.Tests/ExampleTest.cs
+++
@@ -1,9 +0,0 @@
-public class ExampleTest
-{
-    [Fact]
-    public async Task TestBasicConnectivity()
-    {
-        // Smoke test
-        await Example.Run(Environment.GetEnvironmentVariable("CLUSTER_ENDPOINT") ?? "");
-    }
-}
