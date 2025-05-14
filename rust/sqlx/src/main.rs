e419c78afbe28f889b513c32266c8045d235a14c
Daniel Frankcom <daniel@frankcom.ca>
2025-05-14 15:04:46 -0700

    Update Rust example to include non-admin logic and align with docs (#131)

--- rust/sqlx/src/main.rs
+++ rust/sqlx/src/main.rs
@@ -1,120 +1,157 @@
-use std::time::Duration;
-
-use aws_config::{BehaviorVersion, Region};
-use aws_sdk_dsql::auth_token::{AuthTokenGenerator, Config};
-use rand::Rng;
+use aws_config::{BehaviorVersion, Region, SdkConfig};
+use aws_sdk_dsql::auth_token::{AuthToken, AuthTokenGenerator, Config};
 use sqlx::postgres::{PgConnectOptions, PgPoolOptions};
-use sqlx::Row;
+use sqlx::{Pool, Postgres, Row};
+use std::time::Duration;
 use tokio::time;
-use uuid::Uuid;
 
-async fn example(cluster_endpoint: String, region: String) -> anyhow::Result<()> {
-    // Generate auth token
+const SECONDS_PER_MINUTE: u64 = 60;
+
+const TOKEN_EXPIRATION_MINUTES: u64 = 15;
+const TOKEN_EXPIRATION_SECONDS: u64 = TOKEN_EXPIRATION_MINUTES * SECONDS_PER_MINUTE;
+
+const TOKEN_REFRESH_MINUTES: u64 = TOKEN_EXPIRATION_MINUTES - 5;
+const TOKEN_REFRESH_SECONDS: u64 = TOKEN_REFRESH_MINUTES * SECONDS_PER_MINUTE;
+
+const _: () = assert!(
+    TOKEN_EXPIRATION_MINUTES > TOKEN_REFRESH_MINUTES,
+    "Token expiration time must be greater than refresh time"
+);
+
+async fn generate_password_token(
+    cluster_user: &str,
+    signer: &AuthTokenGenerator,
+    sdk_config: &SdkConfig,
+) -> AuthToken {
+    if cluster_user == "admin" {
+        signer
+            .db_connect_admin_auth_token(sdk_config)
+            .await
+            .unwrap()
+    } else {
+        signer.db_connect_auth_token(sdk_config).await.unwrap()
+    }
+}
+
+/// Established a pooled connection with periodic credential refresh.
+async fn establish_connection(
+    cluster_user: String,
+    cluster_endpoint: String,
+    region: String,
+) -> anyhow::Result<Pool<Postgres>> {
     let sdk_config = aws_config::load_defaults(BehaviorVersion::latest()).await;
     let signer = AuthTokenGenerator::new(
         Config::builder()
             .hostname(&cluster_endpoint)
             .region(Region::new(region))
-            .expires_in(900)
+            .expires_in(TOKEN_EXPIRATION_SECONDS)
             .build()
             .unwrap(),
     );
-    let password_token = signer
-        .db_connect_admin_auth_token(&sdk_config)
-        .await
-        .unwrap();
 
-    // Setup connections
+    let password_token = generate_password_token(&cluster_user, &signer, &sdk_config).await;
+    let schema = match cluster_user.as_str() {
+        "admin" => "public",
+        _ => "myschema",
+    };
+
     let connection_options = PgConnectOptions::new()
-        .host(cluster_endpoint.as_str())
+        .host(&cluster_endpoint)
         .port(5432)
         .database("postgres")
-        .username("admin")
+        .username(&cluster_user)
         .password(password_token.as_str())
         .ssl_mode(sqlx::postgres::PgSslMode::VerifyFull);
 
     let pool = PgPoolOptions::new()
         .max_connections(10)
+        .after_connect(move |conn, _meta| {
+            Box::pin(async move {
+                sqlx::query(format!("SET search_path = {}", schema).as_str())
+                    .execute(conn)
+                    .await
+                    .map(|_| ())
+            })
+        })
         .connect_with(connection_options.clone())
         .await?;
 
-    // XXX: Periodically refresh the password by regenerating the token. This
-    // runs every 10 minutes and provides a token valid for 15 minutes.
+    // Periodically refresh the password by regenerating the token. This runs every
+    // TOKEN_REFRESH_MINUTES and provides a token valid for TOKEN_EXPIRATION_MINUTES.
     let _pool = pool.clone(); // Pool uses an Arc internally
     tokio::spawn(async move {
         loop {
-            time::sleep(Duration::from_secs(600)).await;
-            let password_token = signer
-                .db_connect_admin_auth_token(&sdk_config)
-                .await
-                .unwrap();
+            time::sleep(Duration::from_secs(TOKEN_REFRESH_SECONDS)).await;
+
+            let password_token = generate_password_token(&cluster_user, &signer, &sdk_config).await;
             let connect_options_with_new_token =
                 connection_options.clone().password(password_token.as_str());
             _pool.set_connect_options(connect_options_with_new_token);
         }
     });
 
-    // Create owners table
-    // To avoid Optimistic concurrency control (OCC) conflicts
-    // Have this table created already.
+    Ok(pool)
+}
+
+/// Run some example operations against the provided pool.
+async fn exercise_connection(pool: &Pool<Postgres>) -> anyhow::Result<()> {
     sqlx::query(
-        "CREATE TABLE IF NOT EXISTS owner (
-			id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-			name VARCHAR(255),
-			city VARCHAR(255),
-			telephone VARCHAR(255)
-		)").execute(&pool).await?;    
-    
+        r#"
+        CREATE TABLE IF NOT EXISTS owner (
+            id UUID NOT NULL DEFAULT gen_random_uuid(),
+            name VARCHAR(30) NOT NULL,
+            city VARCHAR(80) NOT NULL,
+            telephone VARCHAR(20) DEFAULT NULL,
+            PRIMARY KEY (id)
+        )"#,
+    )
+    .execute(pool)
+    .await?;
+
     // Insert some data
-    let id = Uuid::new_v4();
-    let telephone = rand::thread_rng()
-        .gen_range(123456..987654)
-        .to_string();
-    let result = sqlx::query("INSERT INTO owner (id, name, city, telephone) VALUES ($1, $2, $3, $4)")
-        .bind(id)
+    sqlx::query("INSERT INTO owner (name, city, telephone) VALUES ($1, $2, $3)")
         .bind("John Doe")
         .bind("Anytown")
-        .bind(telephone.as_str())
-        .execute(&pool)
+        .bind("555-555-1999")
+        .execute(pool)
         .await?;
-    assert_eq!(result.rows_affected(), 1);
 
-    // Read data back
-    let rows = sqlx::query("SELECT * FROM owner WHERE id=$1").bind(id).fetch_all(&pool).await?;
-    println!("{:?}", rows);
+    let rows = sqlx::query("SELECT * FROM owner WHERE name=$1")
+        .bind("John Doe")
+        .fetch_all(pool)
+        .await?;
 
+    // Verify the result is what we inserted before
     assert_eq!(rows.len(), 1);
     let row = &rows[0];
     assert_eq!(row.try_get::<&str, _>("name")?, "John Doe");
     assert_eq!(row.try_get::<&str, _>("city")?, "Anytown");
-    assert_eq!(row.try_get::<&str, _>("telephone")?, telephone);
+    assert_eq!(row.try_get::<&str, _>("telephone")?, "555-555-1999");
 
-    sqlx::query("DELETE FROM owner WHERE name='John Doe'")
-        .execute(&pool).await?;
+    // Clean up the table after the example. If we run the example again we do not have to worry
+    // about data inserted by previous runs
+    sqlx::query("DELETE FROM owner WHERE name = 'John Doe'")
+        .execute(pool)
+        .await?;
 
-    pool.close().await;
     Ok(())
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let cluster_endpoint = std::env::var("CLUSTER_ENDPOINT").unwrap();
-    let region = std::env::var("REGION").unwrap();
-    Ok(example(cluster_endpoint, region).await?)
-}
+pub async fn main() -> Result<(), anyhow::Result<()>> {
+    let cluster_user =
+        std::env::var("CLUSTER_USER").expect("env variable `CLUSTER_USER` should be set");
+    let cluster_endpoint =
+        std::env::var("CLUSTER_ENDPOINT").expect("env variable `CLUSTER_ENDPOINT` should be set");
+    let region = std::env::var("REGION").expect("env variable `REGION` should be set");
 
-#[cfg(test)]
-mod tests {
+    let pool = establish_connection(cluster_user, cluster_endpoint, region)
+        .await
+        .unwrap();
+    exercise_connection(&pool).await.unwrap();
+    pool.close().await;
 
-    use super::*;
-    use tokio::test;
+    println!("Connection exercised successfully");
 
-    #[test]
-    async fn test_crud() {
-        let cluster_endpoint = std::env::var("CLUSTER_ENDPOINT").unwrap();
-        let region = std::env::var("REGION").unwrap();
-        let result = example(cluster_endpoint, region).await;
-        assert!(result.is_ok());
-        println!("Successfully completed test run.");
-    }
+    Ok(())
 }
