3eec217050e0a5e4f2bb3cca549d53f86ad74800
amaksimo <247455160+amaksimo@users.noreply.github.com>
2025-12-27 00:22:43 +0000

    Sync examples from connectors

--- javascript/node-postgres/test/alternatives/pool/example_with_nonconcurrent_connection_pool.test.js
+++ javascript/node-postgres/test/alternatives/pool/example_with_nonconcurrent_connection_pool.test.js
@@ -1,5 +1,11 @@
-import { example } from '../../../src/alternatives/pool/example_with_nonconcurrent_connection_pool.js';
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
-test('Smoke test - example_with_nonconcurrent_connection_pool', async () => {
+import { test } from "@jest/globals";
+import { example } from "../../../src/alternatives/pool/example_with_nonconcurrent_connection_pool.js";
+
+test("Example with nonconcurrent connection pool", async () => {
   await example();
-}, 30000);
+});
