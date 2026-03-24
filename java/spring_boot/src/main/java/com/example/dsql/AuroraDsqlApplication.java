ffa1b0c845171d64d1d9fe0a48b9e37a1415e01d
Mirron Panicker <pmirron@amazon.com>
2026-03-24 18:05:45 -0500

    Address PR review: align HikariCP config, fix retry ordering, add integration tests

--- java/spring_boot/src/main/java/com/example/dsql/AuroraDsqlApplication.java
+++ java/spring_boot/src/main/java/com/example/dsql/AuroraDsqlApplication.java
@@ -2,10 +2,11 @@ package com.example.dsql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.Ordered;
 import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
-@EnableRetry
+@EnableRetry(order = Ordered.LOWEST_PRECEDENCE - 1)
 public class AuroraDsqlApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuroraDsqlApplication.class, args);
