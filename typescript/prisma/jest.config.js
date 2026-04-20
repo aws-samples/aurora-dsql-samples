be84151bfb5ef7358bddf07a50dc74e9f8aa7e80
Mirron Panicker <pmirron@amazon.com>
2026-04-20 12:29:20 -0500

    Replace TypeScript Prisma example with multi-region version
    Co-authored-by: Shashwath Raghavan <shashwar@amazon.com>

--- typescript/prisma/jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import("jest").Config} **/
-module.exports = {
-    testEnvironment: "node",
-    testMatch: ["**/dist/test/**/*.test.js"],
-    testTimeout: 60000,
-    moduleNameMapper: {
-        "^@generated/(.*)$": "<rootDir>/dist/generated/$1",
-    },
-};
