6039ca28b03d3becf1da861aefa4174dbc53c205
Raluca Constantin <ralconst@amazon.com>
2026-06-23 17:48:40 -0700

    Remove unused javax.sql.DataSource import from Main.java

--- java/batch_operations/src/main/java/com/example/dsql/batch_operations/Main.java
+++ java/batch_operations/src/main/java/com/example/dsql/batch_operations/Main.java
@@ -9,7 +9,6 @@ import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Logger;
-import javax.sql.DataSource;
 
 /**
  * Main entry point for Aurora DSQL batch operations demo (Java).
